64fb73adcb1152e2c66a17f7ff7003e683c846a7
liquidz <liquidz.uo@gmail.com>
2024-11-16 07:19:08 +0900

    feat: Add modify-code-interceptor

--- elin/elin_format/core.clj
+++ elin/elin_format/core.clj
@@ -20,27 +20,38 @@
     (catch Exception ex
       (e/fault {:message (ex-message ex)}))))
 
-(defn- get-root-dir
+(defn- get-root-dir*
   [host]
   (e/let [cwd (async/<!! (e.p.host/get-current-working-directory! host))]
     (or (e.u.file/get-project-root-directory cwd)
         ".")))
 
+(def ^:private get-root-dir
+  (memoize get-root-dir*))
+
 (def format-current-form-interceptor
-  (let [cached-root-dir (atom nil)]
-    {:kind e.c.interceptor/autocmd
-     :enter (-> (fn [{:as ctx :component/keys [host]}]
-                  (e/let [{:keys [command]} (e.u.interceptor/config ctx #'format-current-form-interceptor)
-                          _ (when (empty? command)
-                              (e/fault {:message "No command is configured"}))
-                          root-dir (or @cached-root-dir
-                                       (let [root-dir' (get-root-dir host)]
-                                         (reset! cached-root-dir root-dir')
-                                         root-dir'))
-                          {cur-lnum :lnum cur-col :col} (async/<!! (e.p.host/get-cursor-position! host))
-                          {:keys [code lnum col]} (e.f.sexpr/get-top-list ctx cur-lnum cur-col)
-                          formatted-code (format-by-command command root-dir code)]
-                    (e.f.sexpr/replace-list-sexpr ctx lnum col formatted-code)))
-                (ix/when #(and (= "BufWritePre" (:autocmd-type %))
-                               (e.u.interceptor/connected? %)))
-                (ix/discard))}))
+  {:kind e.c.interceptor/autocmd
+   :enter (-> (fn [{:as ctx :component/keys [host]}]
+                (e/let [{:keys [command]} (e.u.interceptor/config ctx #'format-current-form-interceptor)
+                        _ (when (empty? command)
+                            (e/fault {:message "No command is configured"}))
+                        root-dir (get-root-dir host)
+                        {cur-lnum :lnum cur-col :col} (async/<!! (e.p.host/get-cursor-position! host))
+                        {:keys [code lnum col]} (e.f.sexpr/get-top-list ctx cur-lnum cur-col)
+                        formatted-code (format-by-command command root-dir code)]
+                  (e.f.sexpr/replace-list-sexpr ctx lnum col formatted-code)))
+              (ix/when #(and (= "BufWritePre" (:autocmd-type %))
+                             (e.u.interceptor/connected? %)))
+              (ix/discard))})
+
+(def modify-code-interceptor
+  {:kind e.c.interceptor/modify-code
+   :leave (fn [{:as ctx :component/keys [host] :keys [result code]}]
+            (if-not result
+              ctx
+              (e/let [{:keys [command]} (e.u.interceptor/config ctx #'modify-code-interceptor)
+                      _ (when (empty? command)
+                          (e/fault {:message "No command is configured"}))
+                      root-dir (get-root-dir host)
+                      formatted-code (format-by-command command root-dir code)]
+                (assoc ctx :code formatted-code))))})
